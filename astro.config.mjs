4e19d4e8e7bf8ac6de2e33ec93482d36b71c110e
Sachin KM <hiscan99@gmail.com>
2025-10-22 18:39:48 +0200

    Update base path in Astro configuration for correct deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://sachinkmohan.github.io",
-  base: "/sachinkmohan.github.io",
+  base: "/",
   vite: {
     plugins: [tailwindcss()],
   },
